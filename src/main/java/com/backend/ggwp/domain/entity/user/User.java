3d3ad6d847bc0e859358ff30b6a29d55f74d5ff4
ParangBird <seekerman@naver.com>
2021-12-30 23:24:45 +0900

    User entity 생성 - react 공부좀

---
+++ src/main/java/com/backend/ggwp/domain/entity/user/User.java
@@ -0,0 +1,23 @@
+package com.backend.ggwp.domain.entity.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String userName;
+    private String password;
+    private String email;
+}
