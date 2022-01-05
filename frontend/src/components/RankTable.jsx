970833693b2467cb78ec2138f1d55f6ef578d129
ParangBird <seekerman@naver.com>
2022-01-05 22:08:48 +0900

    ranking page 조금 - 이전 다음 버튼 추가

--- frontend/src/components/RankTable.jsx
+++ frontend/src/components/RankTable.jsx
@@ -4,29 +4,42 @@ import axios from 'axios'
 export default function RankTable() {
 
     const [rankInfo, setRankInfo] = useState([])
-    //id, summonerId, summonerName, leaguePoints, summmonerRank, wins, losses, veteran, inactive, freshBlood, hotStreak
-
+    const [startRank, setStartRank] = useState(1)
     useEffect(
         () => {
-            axios.get('http://localhost:8080/api/rank/1')
+            axios.get('http://localhost:8080/api/rank/' + startRank)
                 .then(res => {
                     setRankInfo(res.data);
                 }).catch((err) => {
                 console.log('에러')
             })
         }
-    , [])
+    , [rankInfo])
+
+
+    const goPrev = () => {
+        console.log('go prev')
+        if(startRank >= 51)
+            setStartRank(startRank-50)
+    }
+
+    const goNext = () => {
+        console.log('go next')
+        setStartRank(startRank+50);
+    }
 
     return (
         <div>
             {rankInfo.map((rank, index) => (
                 <div>
-                    <span key={index}>
+                    <span key={rank.summonerId}>
                       {rank.summonerName}
                     </span>
                     <br/>
                 </div>
             ))}
+
+            <button onClick={goPrev}>이전</button> <button onClick={goNext}>다음</button>
         </div>
     )
 }
--- frontend/src/pages/RankingPage.jsx
+++ frontend/src/pages/RankingPage.jsx
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import RankTable from "../components/RankTable";
 
 export default function(){
-
-
-
     return(
         <>
             <h1>ranking page</h1>
