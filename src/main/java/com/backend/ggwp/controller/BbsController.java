911b5ac582e021af7b557e43a12a3659a92e6c98
ParangBird <seekerman@naver.com>
2022-08-12 21:20:31 +0900

    구글 로그인 기능 - oauth 환경설정 - 로그아웃

--- src/main/java/com/backend/ggwp/controller/BbsController.java
+++ src/main/java/com/backend/ggwp/controller/BbsController.java
@@ -1,18 +1,35 @@
 package com.backend.ggwp.controller;
 
+import com.backend.ggwp.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/bbs")
+@RequiredArgsConstructor
 public class BbsController {
+
+    private final HttpSession httpSession;
+
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "bbs/index";
     }
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "bbs/login";
     }
 }
