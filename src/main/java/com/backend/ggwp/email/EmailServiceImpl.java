886d2d6aab46c02cbc608c1bbe6ffccb1030ecbc
ParangBird <seekerman@naver.com>
2022-11-25 23:16:40 +0900

    feat: EmailServiceImpl

--- src/main/java/com/backend/ggwp/email/EmailServiceImpl.java
+++ src/main/java/com/backend/ggwp/email/EmailServiceImpl.java
@@ -2,12 +2,14 @@ package com.backend.ggwp.email;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Random;
 
 @Slf4j
 @Service
@@ -22,7 +24,7 @@ public class EmailServiceImpl implements EmailService {
         log.info("인증 번호 : {}", ePw);
         MimeMessage message = emailSender.createMimeMessage();
         message.addRecipients(Message.RecipientType.TO, to);
-        message.setSubject("GGWP 비밀번호 찾기");
+        message.setSubject("GGWP 회원가입 이메일 인증");
 
         String msg = "";
         msg += "<div style='margin:100px;'>";
@@ -39,18 +41,46 @@ public class EmailServiceImpl implements EmailService {
         msg += ePw + "</strong><div><br/> ";
         msg += "</div>";
         message.setText(msg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("properties email쓰세용!", "Babble"));//보내는 사람
+        message.setFrom(new InternetAddress("properties email", "Babble"));//보내는 사람
         return message;
     }
 
+    public static String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+
+            switch (index) {
+                case 0:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                    break;
+                case 1:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    //  A~Z
+                    break;
+                case 2:
+                    key.append((rnd.nextInt(10)));
+                    // 0~9
+                    break;
+            }
+        }
+
+        return key.toString();
+    }
 
     @Override
     public String sendSimpleMessage(String to) throws Exception {
-        return null;
-    }
-
-    private static String createKey() {
-        return null;
+        MimeMessage message = createMessage(to);
+        try {
+            emailSender.send(message);
+        } catch (MailException es) {
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw;
     }
 
 }
