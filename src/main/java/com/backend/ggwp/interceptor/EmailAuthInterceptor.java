c3fc01925ee5cd9b999c93dc5274c111be6d2e10
ParangBird <seekerman@naver.com>
2022-12-04 20:37:27 +0900

    feat: email 미인증 계정 인증 링크 띄우기

---
+++ src/main/java/com/backend/ggwp/interceptor/EmailAuthInterceptor.java
@@ -0,0 +1,21 @@
+package com.backend.ggwp.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class EmailAuthInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
