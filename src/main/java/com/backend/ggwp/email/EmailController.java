add9a83a0be1c1123256c57c7a1d39cd58bfbf9f
ParangBird <seekerman@naver.com>
2022-12-06 20:30:19 +0900

    chore: email api 및 templates 디렉터리 구조 변경

--- src/main/java/com/backend/ggwp/email/EmailController.java
+++ src/main/java/com/backend/ggwp/email/EmailController.java
@@ -2,7 +2,6 @@ package com.backend.ggwp.email;
 
 import com.backend.ggwp.domain.user.GgwpUser;
 import com.backend.ggwp.domain.user.UserService;
-import com.backend.ggwp.domain.user.dto.ResetPasswordDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,13 +12,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 
@@ -43,7 +40,7 @@ public class EmailController {
 
         EmailAuthDto emailAuthDto = new EmailAuthDto();
         model.addAttribute("emailAuthDto", emailAuthDto);
-        return "bbs/email-send";
+        return "email/email-send";
     }
 
     @PostMapping("/bbs/email/send")
@@ -55,17 +52,17 @@ public class EmailController {
             for (ObjectError error : allErrors) {
                 log.info("email dto error : {} ", error);
             }
-            return "bbs/email-send";
+            return "email/email-send";
         }
         String email = emailAuthDto.getEmail();
         Optional<GgwpUser> byEmail = userService.findByEmail(email);
         if (byEmail == null || byEmail.isEmpty()) {
             bindingResult.rejectValue("email", "", "해당 이메일 정보가 없습니다.");
-            return "bbs/email-send";
+            return "email/email-send";
         }
         if (byEmail.get().isEmailAuth()) {
             bindingResult.rejectValue("email", "", "이미 인증된 이메일입니다.");
-            return "bbs/email-send";
+            return "email/email-send";
         }
         log.info("email to {}", email);
         String authString = emailService.sendSimpleMessage(email);
@@ -78,7 +75,7 @@ public class EmailController {
     public String emailAuthPage(@ModelAttribute("emailAuthDto") EmailAuthDto emailAuthDto, HttpSession session) {
         log.info("emailAuthDto 전달 : {} 과 {} ", emailAuthDto.getEmail(), emailAuthDto.getAuthString());
         session.setAttribute("emailAuthDto", emailAuthDto);
-        return "bbs/email-auth";
+        return "email/email-auth";
     }
 
     @PostMapping("/bbs/email/auth")
@@ -96,7 +93,7 @@ public class EmailController {
             PrintWriter out = response.getWriter();
             out.println("<script>alert('인증번호가 틀립니다'); </script>");
             out.flush();
-            return "bbs/email-auth";
+            return "email/email-auth";
         }
         GgwpUser authedUser = userService.findByEmail(emailAuthDto.getEmail()).get();
         authedUser.setEmailAuth(true);
