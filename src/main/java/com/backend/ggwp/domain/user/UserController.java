e05d9669a9653813dbe12752f794b895b47d3dad
ParangBird <seekerman@naver.com>
2022-12-11 21:47:41 +0900

    feat: 회원가입 시 비밀번호 암호화 및 로그인 기능

--- src/main/java/com/backend/ggwp/domain/user/UserController.java
+++ src/main/java/com/backend/ggwp/domain/user/UserController.java
@@ -1,12 +1,11 @@
 package com.backend.ggwp.domain.user;
 
-import com.backend.ggwp.domain.user.GgwpUser;
 import com.backend.ggwp.domain.user.dto.LoginDto;
 import com.backend.ggwp.domain.user.dto.RegisterDto;
 import com.backend.ggwp.domain.user.dto.ResetPasswordDto;
-import com.backend.ggwp.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,8 +23,9 @@ import java.util.Optional;
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-    private final HttpSession httpSession;
+
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/bbs/login")
     public String login(@Validated @ModelAttribute("loginDto") LoginDto loginDto,
@@ -35,9 +35,12 @@ public class UserController {
         String password = loginDto.getPassword();
         System.out.println("password = " + password);
         Optional<GgwpUser> user = userService.findByEmail(email);
+        // .matches(평문의 오리지널 패스워드, 암호화된 패스워드)
         if (user.isEmpty() ||
                 (user != null && (user.get().getPassword() == null ||
-                        !user.get().getPassword().equals(password)))) {
+                        !passwordEncoder.matches(password, user.get().getPassword())))) {
+            log.info("로그인 실패");
+            log.info("{}, {}", user.get().getPassword(), password);
             return "redirect:/bbs";
         }
         HttpSession session = request.getSession();
@@ -46,33 +49,6 @@ public class UserController {
         return "redirect:/bbs";
     }
 
-    @GetMapping("/bbs/reset-password")
-    public String resetPasswordPage(Model model) {
-        model.addAttribute("resetPasswordDto", new ResetPasswordDto());
-        return "bbs/reset-password";
-    }
-
-    @PostMapping("/bbs/reset-password")
-    public String resetPassword(@Validated @ModelAttribute("resetPasswordDto")
-                                        ResetPasswordDto resetPasswordDto,
-                                BindingResult bindingResult) {
-        System.out.println("resetPasswordDto = " + resetPasswordDto.getEmail());
-        if (bindingResult.hasErrors()) {
-            List<ObjectError> allErrors = bindingResult.getAllErrors();
-            for (ObjectError allError : allErrors) {
-                log.info("Error = {}", allError);
-            }
-            log.info("retry to reset-password");
-            return "bbs/reset-password";
-        }
-        Optional<GgwpUser> byEmail = userService.findByEmail(resetPasswordDto.getEmail());
-        if(byEmail == null || byEmail.isEmpty()){
-            bindingResult.rejectValue("email", "", "회원정보에 해당 이메일이 존재하지 않습니다.");
-            return "bbs/reset-password";
-        }
-        log.info("이메일 보냄");
-        return "redirect:/bbs";
-    }
 
     @GetMapping("/bbs/register")
     public String register(Model model) {
@@ -82,7 +58,7 @@ public class UserController {
 
     @PostMapping("/bbs/register")
     public String register(@Validated @ModelAttribute("registerDto") RegisterDto registerDto,
-                           BindingResult bindingResult, HttpServletResponse response) {
+                           BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             List<ObjectError> allErrors = bindingResult.getAllErrors();
@@ -114,11 +90,13 @@ public class UserController {
 
         String userName = registerDto.getUserName();
         String password = registerDto.getPassword();
+        String encodedPassword = passwordEncoder.encode(password);
         String email = registerDto.getEmail();
+
         GgwpUser newGgwpUser = GgwpUser.
                 builder().
                 name(userName).
-                password(password).
+                password(encodedPassword).
                 email(email).
                 build();
 
@@ -130,5 +108,34 @@ public class UserController {
     public String logout() {
         return "bbs/index";
     }
+
+    @GetMapping("/bbs/reset-password")
+    public String resetPasswordPage(Model model) {
+        model.addAttribute("resetPasswordDto", new ResetPasswordDto());
+        return "bbs/reset-password";
+    }
+
+    @PostMapping("/bbs/reset-password")
+    public String resetPassword(@Validated @ModelAttribute("resetPasswordDto")
+                                        ResetPasswordDto resetPasswordDto,
+                                BindingResult bindingResult) {
+        System.out.println("resetPasswordDto = " + resetPasswordDto.getEmail());
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            for (ObjectError allError : allErrors) {
+                log.info("Error = {}", allError);
+            }
+            log.info("retry to reset-password");
+            return "bbs/reset-password";
+        }
+        Optional<GgwpUser> byEmail = userService.findByEmail(resetPasswordDto.getEmail());
+        if (byEmail == null || byEmail.isEmpty()) {
+            bindingResult.rejectValue("email", "", "회원정보에 해당 이메일이 존재하지 않습니다.");
+            return "bbs/reset-password";
+        }
+        log.info("이메일 보냄");
+        return "redirect:/bbs";
+    }
+
 
 }
