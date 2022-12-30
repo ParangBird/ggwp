f35aa30ec382b0296702fa4f692fdd86280a6814
ParangBird <seekerman@naver.com>
2022-12-30 21:35:23 +0900

    feat: 댓글 comment 객체 정의

---
+++ src/main/java/com/backend/ggwp/domain/comment/Comment.java
@@ -0,0 +1,40 @@
+package com.backend.ggwp.domain.comment;
+
+import com.backend.ggwp.domain.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String comment;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private String createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private String modifiedDate;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "post_author")
+    private String postAuthor;
+}
