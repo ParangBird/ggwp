7d2ca7a253b3dbacdacbb742971f343f35954989
ParangBird <seekerman@naver.com>
2023-02-25 00:11:44 +0900

    feat: modelMapper dependency 추가 및 빈 등록

---
+++ src/main/java/com/backend/ggwp/config/BeanConfig.java
@@ -0,0 +1,18 @@
+package com.backend.ggwp.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.
+                getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+}
