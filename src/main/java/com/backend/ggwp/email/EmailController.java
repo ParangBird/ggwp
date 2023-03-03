b8a31fba30030347007f018575d45741ea72a7b8
ParangBird <seekerman@naver.com>
2023-03-03 19:52:51 +0900

    refactor: emailSend

--- src/main/java/com/backend/ggwp/email/EmailController.java
+++ src/main/java/com/backend/ggwp/email/EmailController.java
@@ -1,11 +1,21 @@
 package com.backend.ggwp.email;
 
+import com.backend.ggwp.domain.user.GgwpUser;
 import com.backend.ggwp.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -22,8 +32,8 @@ public class EmailController {
         return "email/email-send";
     }
 
-    /*@PostMapping("/bbs/email/send")
-    public String emailSend(@Validated @ModelAttribute("emailAuthDto") EmailAuthDTO emailAuthDto,
+    @PostMapping("/bbs/email/send")
+    public String emailSend(@Validated @ModelAttribute("emailAuthDTO") EmailAuthDTO emailAuthDTO,
                             BindingResult bindingResult,
                             RedirectAttributes ra) throws Exception {
         if (bindingResult.hasErrors()) {
@@ -33,29 +43,32 @@ public class EmailController {
             }
             return "email/email-send";
         }
-        String email = emailAuthDto.getEmail();
-        Optional<GgwpUser> byEmail = userService.findByEmail(email);
-        if (byEmail == null || byEmail.isEmpty()) {
-            bindingResult.rejectValue("email", "", "해당 이메일 정보가 없습니다.");
-            return "email/email-send";
-        }
-        if (byEmail.get().isEmailAuth()) {
-            bindingResult.rejectValue("email", "", "이미 인증된 이메일입니다.");
+        String email = validEmailCheck(emailAuthDTO, bindingResult);
+        if (email == null) {
             return "email/email-send";
         }
         log.info("email to {}", email);
         String authString = emailService.sendSimpleMessage(email);
-        emailAuthDto.setAuthString(authString);
-        ra.addFlashAttribute("emailAuthDto", emailAuthDto);
+        emailAuthDTO.setAuthString(authString);
+        ra.addFlashAttribute("emailAuthDTO", emailAuthDTO);
         return "redirect:/bbs/email/auth";
     }
 
-    @GetMapping("/bbs/email/auth")
-    public String emailAuthPage(@ModelAttribute("emailAuthDto") EmailAuthDTO emailAuthDto, HttpSession session) {
-        log.info("emailAuthDto 전달 : {} 과 {} ", emailAuthDto.getEmail(), emailAuthDto.getAuthString());
-        session.setAttribute("emailAuthDto", emailAuthDto);
-        return "email/email-auth";
+    private String validEmailCheck(EmailAuthDTO emailAuthDTO, BindingResult bindingResult) {
+        String email = emailAuthDTO.getEmail();
+        Optional<GgwpUser> byEmail = userService.findByEmail(email);
+        if (byEmail == null || byEmail.isEmpty()) {
+            bindingResult.rejectValue("email", "", "해당 이메일 정보가 없습니다.");
+            return null;
+        }
+        if (byEmail.get().isEmailAuth()) {
+            bindingResult.rejectValue("email", "", "이미 인증된 이메일입니다.");
+            return null;
+        }
+        return email;
     }
+
+    /*
 
     @ResponseBody
     @PostMapping("/emailTest")
@@ -63,6 +76,12 @@ public class EmailController {
         log.info("email to {}", email);
         String confirm = emailService.sendSimpleMessage(email);
         return confirm;
+    }
+
+    @GetMapping("/bbs/email/auth")
+    public String emailAuthPage(@ModelAttribute("emailAuthDto") EmailAuthDTO emailAuthDto, HttpSession session) {
+        session.setAttribute("emailAuthDto", emailAuthDto);
+        return "email/email-auth";
     }
 
     @PostMapping("/bbs/email/auth")
