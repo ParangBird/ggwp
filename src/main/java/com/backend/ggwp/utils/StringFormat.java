074f93405531b7bb5b18506362b7e5028a356559
ParangBird <seekerman@naver.com>
2023-03-09 01:02:11 +0900

    fix: StringFormat 수정

--- src/main/java/com/backend/ggwp/utils/StringFormat.java
+++ src/main/java/com/backend/ggwp/utils/StringFormat.java
@@ -1,9 +1,12 @@
 package com.backend.ggwp.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class StringFormat {
     public static String setApiString(String s) {
-        String summonerName = s;
-        if (summonerName.length() == 2) summonerName = summonerName.charAt(0) + " " + summonerName.charAt(1);
-        return summonerName.replace(" ", "+");
+        String summonerName = s.replace(" ", "");
+        summonerName = summonerName.charAt(0) + "+" + summonerName.substring(1);
+        return summonerName;
     }
 }
