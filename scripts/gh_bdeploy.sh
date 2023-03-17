f010dd4fe8c98b52798925e5e228ff57207901ce
ParangBird <seekerman@naver.com>
2023-03-18 01:30:16 +0900

    feat: github-action

--- scripts/gh_bdeploy.sh
+++ scripts/gh_bdeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="ggwp"
-JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/ggwp/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
