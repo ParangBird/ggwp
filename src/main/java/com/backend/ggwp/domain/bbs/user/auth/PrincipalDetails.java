4b0f04c11e51a72a2f33af1fb2d29d85a1d4f33d
ParangBird <seekerman@naver.com>
2023-04-05 23:03:00 +0900

    feat: Spring Security Login

---
+++ src/main/java/com/backend/ggwp/domain/bbs/user/auth/PrincipalDetails.java
@@ -0,0 +1,83 @@
+package com.backend.ggwp.domain.bbs.user.auth;
+
+import com.backend.ggwp.domain.bbs.user.GgwpUser;
+import com.backend.ggwp.domain.bbs.user.dto.GgwpUserDTO;
+import lombok.Getter;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private final ModelMapper modelMapper;
+    private GgwpUser ggwpUser;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(ModelMapper modelMapper, GgwpUserDTO ggwpUserDTO) {
+        this.modelMapper = modelMapper;
+        GgwpUser map = modelMapper.map(ggwpUserDTO, GgwpUser.class);
+        this.ggwpUser = map;
+    }
+
+    public PrincipalDetails(ModelMapper modelMapper, GgwpUserDTO ggwpUserDTO, Map<String, Object> attributes) {
+        this.modelMapper = modelMapper;
+        this.ggwpUser = GgwpUser.builder().email(ggwpUserDTO.getEmail()).build();
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<GrantedAuthority>();
+        collect.add(() -> {
+            if (ggwpUser.getProvider() == null) return "oauth_user";
+            else return "normal_user";
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return ggwpUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return ggwpUser.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/backend/ggwp/domain/bbs/user/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.backend.ggwp.domain.bbs.user.auth;
+
+import com.backend.ggwp.domain.bbs.user.UserService;
+import com.backend.ggwp.domain.bbs.user.dto.GgwpUserDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        GgwpUserDTO byName = userService.findByEmail(username);
+        if (byName == null) return null;
+        else return new PrincipalDetails(modelMapper, byName);
+    }
+}
