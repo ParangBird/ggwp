5b021badf7916880c7e62f69800d4581eb6f8da4
ParangBird <seekerman@naver.com>
2023-04-06 20:50:30 +0900

    feat: ExceptionHandler 안에 error log 찍히게 변경

--- src/main/java/com/backend/ggwp/exception/ExceptionHandler.java
+++ src/main/java/com/backend/ggwp/exception/ExceptionHandler.java
@@ -10,23 +10,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler({NoSuchPostFoundException.class, ApiServerNoSuchDataException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String notfound404() {
-        log.info("404 error");
+    public String notfound404(Exception e) {
+        log.error(e.toString());
         return "error/404";
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler({InvalidApiKeyException.class})
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public String invalidApiKey() {
-        log.info("503 error");
+    public String invalidApiKey(Exception e) {
+        log.error(e.toString());
         return "error/503";
     }
 
 
+    // TODO : 로그 찍히게
     @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class, ApiServerException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String internal500() {
-        log.info("500 error");
+    public String internal500(Exception e) {
+        log.error(e.toString());
         return "error/5xx";
     }
 }
