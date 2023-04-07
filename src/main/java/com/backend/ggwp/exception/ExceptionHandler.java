68c4788339958d677ccfcd9b06847420f0128c43
ParangBird <seekerman@naver.com>
2023-04-07 18:01:17 +0900

    feat: ExceptionHandler 각 익셉션마다 클래스 나눔 + 로그 찍히게

--- src/main/java/com/backend/ggwp/exception/ExceptionHandler.java
+++ src/main/java/com/backend/ggwp/exception/ExceptionHandler.java
@@ -8,9 +8,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Slf4j
 @ControllerAdvice
 public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler({NoSuchPostFoundException.class, ApiServerNoSuchDataException.class})
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoSuchPostFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String notfound404(Exception e) {
+    public String noSuchPost(Exception e) {
+        log.error("해당하는 게시물을 찾을 수 없음");
+        log.error(e.toString());
+        return "error/404";
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ApiServerNoSuchDataException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String noSuchApiData(Exception e) {
+        log.error("해당하는 API 데이터를 찾을 수 없음");
         log.error(e.toString());
         return "error/404";
     }
@@ -18,14 +27,23 @@ public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler({InvalidApiKeyException.class})
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public String invalidApiKey(Exception e) {
+        log.error("현재 API key 가 만료되었음");
         log.error(e.toString());
         return "error/503";
     }
 
-
-    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class, ApiServerException.class})
+    @org.springframework.web.bind.annotation.ExceptionHandler(ApiServerException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String internal500(Exception e) {
+    public String apiServerException(Exception e) {
+        log.error("현재 API 서버에 오류가 있음");
+        log.error(e.toString());
+        return "error/5xx";
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(Exception e) {
+        log.error("정의되지 않은 범용적인 오류");
         log.error(e.toString());
         return "error/5xx";
     }
